8bb507070f0cd8835002a2a0dd51926b6ed6e061
Maxime Mangel <mangel.maxime@protonmail.com>
2024-03-12 09:24:00 +0100

    First commit

---
+++ Program.fs
@@ -0,0 +1,14 @@
+﻿[<AbstractClass>]
+type Reader() =
+    abstract Warnings: ResizeArray<string> with get
+
+let reader =
+    let warnins = ResizeArray<string>()
+    { new Reader() with
+        member __.Warnings = warnins
+    }
+
+reader.Warnings.Add("Warning 1")
+reader.Warnings.Add("Warning 2")
+
+printfn "Count: %A" reader.Warnings.Count
